67c23aec3f78fb37d74471622e2728ff8f3e2721
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-26 00:49:00 +0100

    Fixing ex04 again

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -3,8 +3,8 @@ let arena = new Map();
 
 arena.set("author", "Gigi");
 arena.set("publisher", "Mistral");
-arena.set("numberOfPages", "28");
-arena.set("year", "2021");
+arena.set("numberOfPages", 28);
+arena.set("year", 2021);
 
 // Only change code above this line
 
