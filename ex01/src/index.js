2fe18679f3d12fd0c47b46e4ebf2d2a970cd7c82
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-25 16:23:22 +0100

    Solution for the example 1

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+let myArr = [];
+// Only change code below this line
+function * multiplication(x) {
+    for(let i = 0; i < x; i++) {
+        x *= 2;
+        yield x;
+         myArr.push(x);
+    }
+}
+
+var num = multiplication(3);
+
+for(let i = 0; i < 3; i++){
+    console.log(num.next().value);
+}
+// Only change code above this line
+module.exports = multiplication;
