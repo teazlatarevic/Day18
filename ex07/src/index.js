3e103489b805863f548ebfaeb2578b981c8093c0
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-26 06:09:04 +0100

    Fixing ex07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,11 +1,13 @@
 // Only change code below this line
 let numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
-let mySet = new Set([numberArray]);
+let mySet = new Set([...numberArray]);
 
 let uniqueArray = [];
 for (let item of mySet) {
     uniqueArray.push(item);
-    console.log(uniqueArray);
 }
+uniqueArray.sort((a,b) => a -b)
+console.log(uniqueArray);
+
 // Only change code above this line
 module.exports = { numberArray, uniqueArray};
